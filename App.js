4a7927d946c2158ea7030022652b21ed830e0e68
rgoodman22 <robertgoodman2022@u.northwestern.edu>
2021-01-16 19:30:10 -0600

    fetch data from firebase

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, SafeAreaView, Dimensions } from "react-native";
 import EventSelector from "./components/EventSelector";
 import eventsData from "./public/eventsData";
+import {firebase} from "./utils/firebase";
+
+
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
+
+
 export default function App() {
   const [events, setEvents] = useState(eventsData.events);
+
+  useEffect(() => {
+    const db = firebase.database().ref();
+    const handleData = snap => {
+      if (snap.val()) setEvents(snap.val().events);
+    }
+    db.on('value', handleData, error => alert(error));
+    return () => { db.off('value',handleData); };
+  }, []);
+
+  console.log(events)
 
   return (
     <SafeAreaView style={styles.container}>
