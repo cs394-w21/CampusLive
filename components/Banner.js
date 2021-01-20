9ac88b4af2a1578876d8998e0dfed0902883f802
rgoodman22 <robertgoodman2022@u.northwestern.edu>
2021-01-20 14:59:07 -0600

    banner for my events screen

--- components/Banner.js
+++ components/Banner.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { windowWidth } from "../constants/WindowSize";
 
 const Banner = () => {
   return (
     <View style={styles.header}>
       <Text style={styles.headerText}>{"Northwestern Events"}</Text>
+      <View style={styles.headerButtons}>
+        <TouchableOpacity style={styles.Button}>
+          <Text style={styles.ButtonText}>My Events</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.Button}>
+          <Text style={styles.ButtonText}>Feed</Text>
+        </TouchableOpacity>
+      </View>
+
     </View>
   );
 };
@@ -14,13 +23,38 @@ const styles = StyleSheet.create({
   header: {
     backgroundColor: "#4E2A84",
     width: windowWidth,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
   },
   headerText: {
-    fontFamily: "campton",
+    fontFamily: "tahoma",
     fontSize: 32,
     padding: 10,
     color: "white",
+    paddingLeft: 10,
   },
+  Button: {
+    flex: 1,
+    borderRadius: 5,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    margin: 10,
+    height: 40,
+    padding: 10,
+    minWidth: 120,
+    maxWidth: 120,
+    backgroundColor: '#bb99ff',
+  },
+  headerButtons: {
+    paddingRight: 10,
+    flexDirection: 'row'
+  },
+  ButtonText: {
+    fontFamily: "tahoma",
+    fontSize: 18,
+    color: "white"
+  }
+
 });
 
 export default Banner;
